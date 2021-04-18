0d5fa374fd1acc7875bac55238f4ca62ba5cbaae
bmichalczuk <barmic90@hotmail.com>
2021-04-18 09:32:20 +0200

    init project, add webpack with config and other dependencies. form basic files tructure, add gitignore

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./app/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: "/(node_modules)/",
+                use: {
+                  loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/, 
+                use: ["style-loader",'css-loader',"sass-loader"]
+            },
+        ], 
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "app/index.html"
+        })
+    ]
+
+};
