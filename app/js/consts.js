e54101252aaa8019d86f81560e0c1a55fd5cda95
bmichalczuk <barmic90@hotmail.com>
2021-04-29 23:14:01 +0200

    add area to display results, add file to keep consts, add helper functions to get title and author book from MyClippings.txt from kindle reader

---
+++ app/js/consts.js
@@ -0,0 +1,6 @@
+//parsing books 
+export const entryDivider = "==========";
+export const titleDivider = "- Your Highlight";
+export const authorStargSign = "(";
+export const authorEndSing = ")";
+
--- app/js/domNodes.js
+++ app/js/domNodes.js
@@ -2,4 +2,4 @@ export const dropArea = document.querySelector(".file-box");
 export const fileInpt = document.querySelector(".file-box__input");
 export const fileName = document.querySelector(".file-box__name");
 export const removeBtn = document.querySelector(".file-box__remove");
-
+export const parseBtn = document.querySelector(".button--parse");
