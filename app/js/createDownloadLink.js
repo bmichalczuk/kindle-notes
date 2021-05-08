c0346d542e568cbe9088b65fb21a6789ecafea0e
bmichalczuk <barmic90@hotmail.com>
2021-05-09 01:09:49 +0200

    add styles and icon for download links, add dist to .gitignore, change name of icon

--- app/js/createDownloadLink.js
+++ app/js/createDownloadLink.js
@@ -1,9 +1,12 @@
+import icon from "../img/txtIcon.svg";
+
 const createDownloadLink = ({title, entries}) => {
     const content = entries.join();
     const text = new Blob([content], { type: 'text/plain' });
     
     const a = document.createElement("a");
-    
+    const img = document.createElement("img");
+
     if (window.webkitURL != null) {
         a.href = window.webkitURL.createObjectURL(text);
     } else {
@@ -12,7 +15,12 @@ const createDownloadLink = ({title, entries}) => {
     
     a.download = title;
     a.textContent = title;
-    a.class = "result__download";
+    a.classList.add("download");
+    a.title = "Click to download file";
+    img.classList.add("download__icon");
+    img.src = icon;
+    img.alt = "File icon";
+    a.prepend(img);
     return a;
 };
 
